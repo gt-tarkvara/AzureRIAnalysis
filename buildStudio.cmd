fa5bd0e9e3ef5a74af093a1a35635932b9d3e0fd
Siim Aus <herrbpl@gmail.com>
2019-06-03 17:27:47 +0300

    Buildstudio update

--- buildStudio.cmd
+++ buildStudio.cmd
@@ -1,2 +1,3 @@
-docker build -t herrbpl/rstudio-tidyverse -f Dockerfile.RStudio.intermediate .
-docker tag herrbpl/rstudio-tidyverse herrbpl/rstudio-tidyverse:latest
+docker build -t rstudio-tidyverse -f Dockerfile.RStudio.intermediate .
+docker tag rstudio-tidyverse:latest herrbpl/rstudio-tidyverse:latest
+docker push herrbpl/rstudio-tidyverse:latest
