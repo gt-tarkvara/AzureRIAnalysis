41ac74c54899a11e4fa8c2bfac1ee127f3a4883d
Siim Aus <herrbpl@gmail.com>
2019-06-14 17:10:59 +0300

    Updated container config

--- 02_code/install_packages.R
+++ 02_code/install_packages.R
@@ -6,6 +6,7 @@ install.packages("rvest")
 # for profiling/debugging
 install.packages("pryr")
 install.packages("odbc")
+install.packages("rprojroot")
 
 #install.packages("httr")
 #install.packages("readr")
