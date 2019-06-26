acf6b61de9ddfeaad5b711c165f4d0bd1504e8d0
Siim Aus <herrbpl@gmail.com>
2019-06-26 17:08:46 +0300

    Changed exec mode

--- 03_dataupdater/run.sh
+++ 03_dataupdater/run.sh
@@ -11,26 +11,28 @@ case $RUNMODE in
 
   ALL)
     echo "Updating all"
-	exec Rscript all.aux.R || exit 1
+	exec Rscript all.aux.R
     ;;
 
   AUX)
     echo "Updating auxiliarity tables only"
-	exec Rscript run.aux.R || exit 1
+	exec Rscript run.aux.R
     ;;
 
   USAGE)
     echo "Updating billingdata and RIUtilization tables only"
-	exec Rscript run.usage.R || exit 1
+	exec Rscript run.usage.R
     ;;
 
   ALL_WITH_REPORT)
     echo "Updating all with HTML report"
 	REPORTNAME=report-$(date +%s).html
-	exec Rscript -e "library(rmarkdown); rmarkdown::render(\"UpdateData.Rmd\", output_file=\"$REPORTNAME\")"	|| exit 1
+	exec Rscript -e "library(rmarkdown); rmarkdown::render(\"UpdateData.Rmd\", output_file=\"$REPORTNAME\")"
 	
+	echo "Done"
 	# send report to azure storage
 	if [ ! -z "$AZURERI_STORAGEACCOUNT" ] && [ ! -z "$AZURERI_STORAGECONTAINER" ] && [ ! -z "$AZURERI_SASTOKEN" ]; then
+		echo "Sending report"
 		curl -X PUT -T ./${REPORTNAME} -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" \
 			"https://${AZURERI_STORAGEACCOUNT}.blob.core.windows.net/${AZURERI_STORAGECONTAINER}/${REPORTNAME}${AZURERI_SASTOKEN}"
 	fi
